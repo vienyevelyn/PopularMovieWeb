e5c97979942bd671516056e684f6bbd0419f9596
vienyevelyn <vieny.evelin@gmail.com>
2025-07-24 23:34:55 +0700

    feat(talent-growth): Responsive + Designing Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -32,16 +32,16 @@ function Profile(){
     return (
         <>
             <div className="bg-gray-400 rounded-md backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-lg p-8">
-                <h1 className="text-3xl font-bold mb-4">
+                <h1 className="text-2xl md:text-4xl font-bold text-center text-white mb-2">
                     Welcome, {data.username}!
                 </h1>
-                <p className="text-lg mb-2">
-                    <span>Email: {data.email}</span> 
+                <p className="text-lg mb-3 text-white">
+                    <span className="font-medium text-gray-300">Email:</span>  {data.email}
                 </p>
-                <button className="text-white" onClick={checkMovie}>
+                <button className="text-white block mx-auto w-full py-2 px-4 bg-blue-600 text-white rounded-full mb-4 hover:bg-blue-700 transition duration-200" onClick={checkMovie}>
                     Check Popular Movie
                 </button>
-                <button className="text-white" onClick={LogingOut}>
+                <button className="text-white block mx-auto w-full py-2 px-4 bg-blue-600 text-white rounded-full mb-4 hover:bg-blue-700 transition duration-200" onClick={LogingOut}>
                     Log Out
                 </button>
             </div>
